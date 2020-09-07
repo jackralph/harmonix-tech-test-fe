b1510dbd2cf5504270da7931597d13423af2a50b
Jack Ralph <jackralph@Jacks-MBP.local>
2020-09-07 19:39:35 +0100

    added guard for numbers in input

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,86 +2,23 @@ import React, { Component } from "react";
 import Results from "./Results";
 import TableHead from "./TableHead";
 import OrderByCheckbox from "./OrderByCheckbox";
-import * as api from "../utils/api";
+import WordComparisonInput from "./WordComparisonInput";
 
 class Table extends Component {
   state = {
     orderDesc: true,
-    firstWord: "",
-    secondWord: "",
   };
 
   changeOrder = () => {
     this.setState({ orderDesc: !this.state.orderDesc });
   };
 
-  handleInput = (event) => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-    console.log(this.state);
-  };
-
-  handleSubmit = () => {
-    const { firstWord, secondWord } = this.state;
-    if (firstWord.length === 0 || secondWord.length === 0) {
-      alert("Both fields are required for comparison");
-    } else {
-      api.newComparison(firstWord, secondWord);
-      this.setState({ firstWord: "", secondWord: "" });
-    }
-  };
-
   render() {
-    const { orderDesc, firstWord, secondWord } = this.state;
+    const { orderDesc } = this.state;
     return (
       <div>
         <OrderByCheckbox changeOrder={this.changeOrder} orderDesc={orderDesc} />
-        <div
-          className='input-group input-group-sm mb-3'
-          style={{ width: "40%", margin: "auto" }}
-        >
-          <div className='input-group-prepend'>
-            <span className='input-group-text' id='inputGroup-sizing-sm'>
-              First word
-            </span>
-          </div>
-          <input
-            type='text'
-            name='firstWord'
-            value={firstWord}
-            className='form-control'
-            aria-label='Small'
-            aria-describedby='inputGroup-sizing-sm'
-            onChange={this.handleInput}
-          />
-        </div>
-        <div
-          className='input-group input-group-sm mb-3'
-          style={{ width: "40%", margin: "auto" }}
-        >
-          <div className='input-group-prepend'>
-            <span className='input-group-text' id='inputGroup-sizing-sm'>
-              Second word
-            </span>
-          </div>
-          <input
-            type='text'
-            name='secondWord'
-            value={secondWord}
-            className='form-control'
-            aria-label='Small'
-            aria-describedby='inputGroup-sizing-sm'
-            onChange={this.handleInput}
-          />
-        </div>
-        <button
-          type='submit'
-          class='btn btn-outline-secondary'
-          style={{ margin: "10px" }}
-          onClick={this.handleSubmit}
-        >
-          Secondary
-        </button>
+        <WordComparisonInput />
         <table className='table'>
           <TableHead />
           <Results orderDesc={orderDesc} />
---
+++ src/components/WordComparisonInput.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import * as api from "../utils/api";
+
+class WordComparisonInput extends Component {
+  state = {
+    firstWord: "",
+    secondWord: "",
+  };
+
+  handleSubmit = () => {
+    const { firstWord, secondWord } = this.state;
+    if (firstWord.length === 0 || secondWord.length === 0) {
+      alert("Both fields are required for comparison");
+    } else if (/\d/.test(firstWord) || /\d/.test(secondWord)) {
+      alert("Numbers are not permitted");
+    } else {
+      api.newComparison(firstWord, secondWord);
+      this.setState({ firstWord: "", secondWord: "" });
+    }
+  };
+
+  handleInput = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    const { firstWord, secondWord } = this.state;
+    return (
+      <div>
+        <div
+          className='input-group input-group-sm mb-3'
+          style={{ width: "40%", margin: "auto" }}
+        >
+          <div className='input-group-prepend'>
+            <span className='input-group-text' id='inputGroup-sizing-sm'>
+              First word
+            </span>
+          </div>
+          <input
+            type='text'
+            name='firstWord'
+            value={firstWord}
+            className='form-control'
+            aria-label='Small'
+            aria-describedby='inputGroup-sizing-sm'
+            onChange={this.handleInput}
+          />
+        </div>
+        <div
+          className='input-group input-group-sm mb-3'
+          style={{ width: "40%", margin: "auto" }}
+        >
+          <div className='input-group-prepend'>
+            <span className='input-group-text' id='inputGroup-sizing-sm'>
+              Second word
+            </span>
+          </div>
+          <input
+            type='text'
+            name='secondWord'
+            value={secondWord}
+            className='form-control'
+            aria-label='Small'
+            aria-describedby='inputGroup-sizing-sm'
+            onChange={this.handleInput}
+          />
+        </div>
+        <button
+          type='submit'
+          className='btn btn-outline-secondary'
+          style={{ margin: "10px" }}
+          onClick={this.handleSubmit}
+        >
+          Submit comparison
+        </button>
+      </div>
+    );
+  }
+}
+
+export default WordComparisonInput;
